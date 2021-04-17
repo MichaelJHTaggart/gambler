1b7480ee7e52508d222203b1a6bc8a4f6a3e0124
Michael Taggart <mtaggarthawk@gmail.com>
2021-04-17 11:43:35 -0600

    changes to server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,18 +2,20 @@ module.exports = {
  spin: async (req, res) => {
   const db = req.app.get('db')
   const id = req.session.user.id
-  const coins = Number(req.session.user.coins)
+  const counter = (db.coin_count(req.session.user.id))
+  const coins = [coins.counter]
+
+  console.log(coins)
 
   if (coins === 0) {
    return response.status(400).send('You do not have enough coins to use the slot machine!')
   }
 
-  function minusOne() {
-   const anAmount = (coins - 1)
+  async function minusOne() {
+   const [anAmount] = await (db.coin_count(req.session.user.id))
+   console.log(anAmount)
    db.spin_cost(anAmount, req.session.user.id)
   }
-
-  minusOne()
 
   const reelOne = ["cherry", "lemon", "apple", "lemon", "banana", "banana", "lemon", "lemon"]
   const reelTwo = ["lemon", "apple", "lemon", "lemon", "cherry", "apple", "banana", "lemon"]
@@ -34,18 +36,22 @@ module.exports = {
    if (string === "cherry") {
     const c3Amount = (50 + coins)
     db.winnings(c3Amount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! This is the big one! Your coin count is: ${c3Amount}`)
    } else if (string === "apple") {
     const a3Amount = (20 + coins)
     db.winnings(a3Amount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! A bushel of apples to sweeten your day! Your coin count is: ${a3Amount}`)
    } else if (string === "banana") {
     const b3Amount = (15 + coins)
+    minusOne()
     db.winnings(b3Amount, id)
     return res.status(200).send(`You WON!!! You are getting ahead of the bunch! Your coin count is: ${b3Amount}`)
    } else if (string === "lemon") {
     const l3Amount = (3 + coins)
     db.winnings(l3Amount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! Three lemons. Actually it doesn't seem like much of a victory... Your coin count is: ${l3Amount}`)
    }
   }
@@ -54,18 +60,22 @@ module.exports = {
    if (string === "cherry") {
     const cAmount = (40 + coins)
     db.winnings(cAmount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! This is the second biggest haul!!! Your coin count is: ${cAmount}`)
    } else if (string === "apple") {
     const aAmount = (10 + coins)
     db.winnings(aAmount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! Try to get 3 now! Your coin count is: ${aAmount}`)
    } else if (string === "banana") {
     const bAmount = (5 + coins)
     db.winnings(bAmount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! Try to get 3 now! Your coin count is: ${bAmount}`)
    } else if (string === "lemon") {
     const lAmount = (0 + coins)
     db.winnings(lAmount, id)
+    minusOne()
     return res.status(200).send(`You WON!!! ...but...there is no reward. Sorry :/ Your coin count is: ${lAmount}`)
    }
   }
@@ -77,6 +87,7 @@ module.exports = {
   } else if (reelTwoAns === reelThreeAns) {
    return twoWins(reelTwoAns)
   } else {
+   minusOne()
    return res.status(200).send(`Sorry, not a winner yet. Try again! ${req.session.user.coins}`)
   }
  }
--- src/Components/SlotMachine.js
+++ src/Components/SlotMachine.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Apple from '../assets/apple.png'
+import Banana from '../assets/banana.png'
+import Lemon from '../assets/lemon.png'
+import Cherry from '../assets/cherry.png'
+import Slot from '../assets/slot.png'
 
 const SlotMachine = props => {
 
@@ -15,13 +20,26 @@ const SlotMachine = props => {
   }
 
   return (<div>
-    <div>
+
+    <div id="slot-wrapper">
       Message: {response}
     </div>
 
-    <button
+    <div>
+      <img class="slot-machine" alt="slot machine" src={Slot} />
+      <img class="slot" alt="apple" src={Apple} />
+      <img class="slot" alt="lemon" src={Lemon} />
+      <img class="slot" alt="cherry" src={Cherry} />
+      <img class="slot" alt="banana" src={Banana} />
+    </div>
+
+
+
+    <button className="spin-button"
       onClick={spin}
-    >Spin</button>
+    >
+      Spin
+    </button>
   </div>
 
   )
